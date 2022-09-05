e3a672a355e8bac08d833628c2f5669fda04118c
Zaur199 <zaur.rojer@mail.ru>
2022-09-05 12:28:36 +0300

    changes done

---
+++ ЗАДАЧА1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.WriteLine("Введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+
+if(k1==k2 && b1!=b2)
+{
+    Console.WriteLine("Две прямые параллельны");
+}
+
+else if(k1==k2 && b1==b2)
+{
+    Console.WriteLine("Две прямые совпадают");
+}
+
+else
+{
+double x = (b2 - b1)/(k1 - k2);
+double y = k1 * x + b1;
+
+
+Console.WriteLine($"Две прямые пересекаются в точке с координатами X:{x}, Y:{y}");
+}
---
+++ ЗАДАЧА3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу копирования массива
+
+int[] array = {4, 8, 3, -7, 0, -9, -3, 1, 1, 2};
+int[] copy = new int[array.Length];
+
+for(int i = 0; i < array.Length; i++)
+{
+    copy[i] = array[i];
+}
+Console.Write("Копия массива:");
+Console.WriteLine(String.Join(", ", copy));
+Console.WriteLine();
